260c1bfa1e09f76264646f56094cf6dfd6d6cce3
gogahyun <45120083+gogahyun@users.noreply.github.com>
2022-07-14 17:53:14 +0900

    Flask API 웹서버 app.py
    /classify로 동영상 제목을 전송했을 경우, 해당 동영상의 유해성 여부를 판별하고 결과를 반환

---
+++ Model/Server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask ,request  
+from flask_restx import Api 
+from classVID import predictVid
+
+app = Flask(__name__)  # Flask 객체 선언
+api = Api(app)  # Flask 객체에 Api 객체 등록
+
+#동영상 유해성 판별 api
+@api.route('/classify',methods=['POST'])
+def classify():
+    video_title=request.form['title'] #동영상 제목 추출
+    result = predictVid(video_title) #해당 동영상 유해성 판별
+    return str(result) #유해성 결과 반환
+
+if __name__=='__main__':
+ app.run(host='0.0.0.0', port=5000, debug=True)
