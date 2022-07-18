28bbbcfd243b5123b5cb03b9ec473f4f02d9ae1d
gogahyun <45120083+gogahyun@users.noreply.github.com>
2022-07-18 23:04:35 +0900

    Update app.py

--- Model/Server/app.py
+++ Model/Server/app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)  # Flask 객체 선언
 api = Api(app)  # Flask 객체에 Api 객체 등록
 
 #동영상 유해성 판별 api
-@api.route('/classify',methods=['POST'])
+@app.route('/classify',methods=['POST'])
 def classify():
     video_title=request.form['title'] #동영상 제목 추출
     result = predictVid(video_title) #해당 동영상 유해성 판별
     return str(result) #유해성 결과 반환
 
 if __name__=='__main__':
- app.run(host='0.0.0.0', port=5000, debug=True)
+ try:
+    app.run(host='0.0.0.0', port=5000, debug=True)
+ except:
+    print('connect error')
