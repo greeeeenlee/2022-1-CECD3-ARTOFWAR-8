cc644c95f509c12855d2195673b316b7bc0e3929
jieun1128 <5656jieun@naver.com>
2022-07-22 13:21:58 +0900

    model 약간 수정 및, 로그인 및 회원가입 serializer 추가

--- Backend/app/core/serializer.py
+++ Backend/app/core/serializer.py
@@ -3,3 +3,12 @@ from rest_framework import serializers
 class videoSerializer(serializers.Serializer):
     videoFile = serializers.FileField()
     name = serializers.CharField()
+
+class loginSerializer(serializers.Serializer):
+    ID = serializers.CharField()
+    pwd = serializers.CharField()
+
+class signUpSerializer(serializers.Serializer):
+    ID = serializers.CharField()
+    pwd = serializers.CharField()
+    name = serializers.CharField()
